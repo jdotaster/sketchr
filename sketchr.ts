d69a897b9796407f65d2be28c0da656ce1e0ca8b
Joshua <asterj64@gmail.com>
2024-07-30 22:17:27 -0600

    add hover event listener on grid

--- sketchr.ts
+++ sketchr.ts
@@ -4,13 +4,16 @@ const loadCanvas = (n) => {
     const canvas = document.getElementById("canvas") || document.createElement("div");
 
     for (let i = 0; i < n; i++) {
-        var canvasRow = document.createElement("div");
+        let canvasRow = document.createElement("div");
         canvasRow.className = "canvasRow";
         canvas.appendChild(canvasRow);
 
         for (let j = 0; j < n; j++) {
-            var canvasSpace = document.createElement("div");
+            let canvasSpace = document.createElement("div");
             canvasSpace.className = "canvasSpace";
+            canvasSpace.addEventListener("mouseover", () => {
+                canvasSpace.style.backgroundColor = "black";
+            })
             canvasRow.appendChild(canvasSpace);
         }
     }
